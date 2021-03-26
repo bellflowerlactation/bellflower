22d43c4ce5079e9e2e1e41e724ece8b717d10e2a
Billy Whited <rocketspops@gmail.com>
2021-03-26 15:08:38 -0500

    Remove uneeded JS library

--- assets/js/bellflower.js
+++
@@ -1,9 +0,0 @@
-document.onreadystatechange = function () {
-    if (document.readyState === 'complete') {
-        const navItems = document.querySelectorAll('.nav-item > a');
-        navItems.forEach(item => {
-            let itemURL = new URL(item.href)
-            itemURL.pathname == window.location.pathname ? item.classList.add('isActive') : null
-        })
-    }
-}
